b06b1649c31e4a0ce3800cd950dd6109b8da9246
TINKU SINGH <154521515+Tinku-Singh1212@users.noreply.github.com>
2024-04-10 13:27:39 +0530

    day 7 10/04/2024

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+// import './App.css'
+import Languages from './component/Languages'
+import Message from './component/Message'
+
+function App() {
+  return (
+    <>
+      {/* <h1>TINKU SINGH</h1>
+      <p>This is a react js</p> */}
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Languages/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Languages.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Languages = () => {
+    let lang=["java","javascript","Kotlin","Python","c"];
+  return (
+    <div>
+        <h1>Best Programming Languages</h1>
+        <ul>
+            {lang.map((item,index)=>(
+              <li key={index}>{item}</li>
+            ))}
+        </ul>
+
+    </div>
+  )
+}
+
+export default Languages
