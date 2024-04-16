8aee56df9e657a343f57dc36a94770d5479486e7
TINKU SINGH <154521515+Tinku-Singh1212@users.noreply.github.com>
2024-04-16 15:29:16 +0530

    day 9 16/04/2024

---
+++ component/Cards.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Cards = (props) => {
+        const { title , description , thumbnail, price , brand } = props;
+    return (
+        <div className='d-flex m-1'>
+                <div className="card" style={{ width: "18rem" }}>
+                    <img className="card-img-top overflow-hidden" src={thumbnail} alt="Card image cap" />
+                    <div className="card-body">
+                            <h5 className="card-title">{title}</h5>
+                             <p className="card-text">
+                                        {description}
+                             </p>
+                             <h5>{price} Rs</h5>
+                             <h6>{brand}</h6>
+                         <div className='d-flex flex-row justify-content-between'></div>
+                             <a href="https://www.instagram.com/" className="btn btn-warning">instagram profile</a>
+                    </div>
+            </div>
+     </div>
+     )
+};
+
+export default Cards;
---
+++ component/Mycomponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const mycomponent = () => {
+  return (
+    <div>mycomponent</div>
+  )
+}
